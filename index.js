84d8fef61e2ed024a388bf1909cd9d7480d2635f
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-07-03 11:30:09 -0400

    adding js functionality part 2, project complete but score system is not working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelectorAll('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -17,3 +20,30 @@ function randomHole(holes) {
     lastHole = hole;
     return hole;
 }
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000) // 10 seconds
+}
+
+function wack(e) {
+    if (!e.isTrusted) return;
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', wack));
